0695ce79fd75ba88a35fc9cb4c3d4c2fba0bf0fa
kazakhokage <membkz@gmail.com>
2025-04-02 03:28:03 +0500

    main

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Steam CS2 Knives beta list')
+
+@section('contents')
+  <div class="row">
+    <p>Hello, this is the beta list of CS2 knives. Proceed to the list section to view available skins.</p>
+    <img src="{{ asset('admin_assets/img/cs2-knife.gif') }}" alt="CS2 Knife GIF">
+  </div>
+@endsection
